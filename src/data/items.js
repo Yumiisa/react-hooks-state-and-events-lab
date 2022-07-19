df9fdb01abd22d73f29a11073a464052607ce34f
caudencia wanyama <conny@students.uonbi.ac.ke>
2022-07-19 20:49:38 +0300

    commit tree 88c5e941a75ff00c4b5590dcb26fa2dbb94ddd6c

--- src/data/items.js
+++ src/data/items.js
@@ -1,3 +1,4 @@
+import React, {useState} from "react";
 const items = [
   { id: 1, name: "Yogurt", category: "Dairy" },
   { id: 2, name: "Pomegranate", category: "Produce" },
@@ -6,4 +7,27 @@ const items = [
   { id: 5, name: "Cookies", category: "Dessert" },
 ];
 
-export default items;
+import React, { useState } from "react";
+
+function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+
+  function handleAddToCartClick() {
+    setIsInCart((isInCart) => !isInCart);
+  }
+
+  return (
+    <li className={isInCart ? "in-cart" : ""}>
+      <span>{name}</span>
+      <span className="category">{category}</span>
+      <button
+        className={isInCart ? "remove" : "add"}
+        onClick={handleAddToCartClick}
+      >
+        {isInCart ? "Remove From" : "Add to"} Cart
+      </button>
+    </li>
+  );
+}
+
+export default Item;
